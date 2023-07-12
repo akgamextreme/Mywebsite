941c8c1ffceed2559b75db84b9a62acf95cfcc3b
AK6969 <66675913+akgamextreme@users.noreply.github.com>
2023-07-12 19:27:55 +0530

    Update weatherjs.js

--- weatherjs.js
+++ weatherjs.js
@@ -1,33 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = '6d40948f959843da84b154932231007';
-  
-    // Get tomorrow's date
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    const tomorrowDate = tomorrow.toISOString().slice(0, 10);
-  
-    // Make a request to the API
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=New Delhi&dt=${tomorrowDate}`)
+  const apiKey = '6d40948f959843da84b154932231007';
+
+  // Get elements
+  const locationSelect = document.getElementById('location');
+  const temperatureElement = document.getElementById('temperature');
+  const rainForecastElement = document.getElementById('rain-forecast');
+
+  // Function to fetch weather data
+  function fetchWeatherData(location) {
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=2`)
       .then(response => response.json())
       .then(data => {
         // Extract temperature and rain forecast for tomorrow
-        const temperature = data.forecast.forecastday[0].day.avgtemp_c;
-        const rainChance = data.forecast.forecastday[0].day.daily_chance_of_rain;
-  
+        const tomorrowData = data.forecast.forecastday[1].day;
+        const temperature = tomorrowData.avgtemp_c;
+        const rainChance = tomorrowData.daily_chance_of_rain;
+
         // Display the temperature and rain forecast on the website
-        const temperatureElement = document.getElementById('temperature');
         temperatureElement.textContent = `Temperature: ${temperature}°C`;
-  
-        const rainForecastElement = document.getElementById('rain-forecast');
         rainForecastElement.textContent = `Rain Forecast: ${rainChance}% chance of rain`;
       })
       .catch(error => {
         // Handle error if the request fails
-        const weatherInfoElement = document.getElementById('weather-info');
-        weatherInfoElement.innerHTML = '<p>Failed to fetch weather forecast.</p>';
+        temperatureElement.textContent = 'Failed to fetch weather data.';
+        rainForecastElement.textContent = '';
       });
-  
-    // Add dark mode class to the body
-    document.body.classList.add('dark-mode');
+  }
+
+  // Event listener for location select change
+  locationSelect.addEventListener('change', function() {
+    const selectedLocation = locationSelect.value;
+    fetchWeatherData(selectedLocation);
   });
-  
+
+  // Fetch weather data for default location
+  fetchWeatherData(locationSelect.value);
+});
